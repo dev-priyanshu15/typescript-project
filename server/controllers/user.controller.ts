0b339ffc11c87ff554c1b24618955ceb9ee319b0
Priyanshu Singh <s.priyanshu.coder@gmail.com>
2024-07-12 01:41:06 +0530

    user activation implementation

--- server/controllers/user.controller.ts
+++ server/controllers/user.controller.ts
@@ -85,3 +85,45 @@ export const createActivationToken = (user: any): IActivationToken => {
   return { token, activationCode };
 };
 
+// activate user
+interface IActivationRequest {
+    activation_token: string;
+    activation_code: string;
+  }
+  
+  export const activateUser = CatchAsyncError(
+    async (req: Request, res: Response, next: NextFunction) => {
+      try {
+        const { activation_token, activation_code } =
+          req.body as IActivationRequest;
+  
+        const newUser: { user: IUser; activationCode: string } = jwt.verify(
+          activation_token,
+          process.env.ACTIVATION_SECRET as string
+        ) as { user: IUser; activationCode: string };
+  
+        if (newUser.activationCode !== activation_code) {
+          return next(new ErrorHandler("Invalid activation code", 400));
+        }
+  
+        const { name, email, password } = newUser.user;
+  
+        const existUser = await userModel.findOne({ email });
+  
+        if (existUser) {
+          return next(new ErrorHandler("Email already exist", 400));
+        }
+        const user = await userModel.create({
+          name,
+          email,
+          password,
+        });
+  
+        res.status(201).json({
+          success: true,
+        });
+      } catch (error: any) {
+        return next(new ErrorHandler(error.message, 400));
+      }
+    }
+  );
